46fe83968e82a08bcecb0937588b2a8e2d84a974
Wanzero-grav <gostroi@mail.ru>
2020-12-25 00:48:19 +0500

    create windows MainActivity and playerActivity and RegisterActivity

--- app/src/main/java/space/sadnov/alenov/dtpapp/playerActivity.java
+++ app/src/main/java/space/sadnov/alenov/dtpapp/playerActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class pedestrian extends AppCompatActivity {
+public class playerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pedestrian);
+        setContentView(R.layout.activity_player);
     }
 }
