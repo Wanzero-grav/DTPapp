128b03594fd1e4daa2314b3a08a836c2687d50d6
Wanzero-grav <gostroi@mail.ru>
2020-12-25 00:51:00 +0500

    creating links between windows

--- app/src/main/java/space/sadnov/alenov/dtpapp/RegisterActivity.java
+++ app/src/main/java/space/sadnov/alenov/dtpapp/RegisterActivity.java
@@ -2,61 +2,13 @@ package space.sadnov.alenov.dtpapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.Spinner;
-import android.widget.TextView;
-
 
 public class RegisterActivity extends AppCompatActivity {
-
-    String[] viewDTP = { "Столкновение", "Наезд на пешехода", "Опрокидывание"};
-    private Button btnnext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
-        btnnext = (Button)findViewById(R.id.btnnext);
-
-        btnnext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent activity_playerIntent = new Intent(RegisterActivity.this, playerActivity.class);
-                startActivity(activity_playerIntent);
-            }
-        });
-
-
-        TextView selection = (TextView) findViewById(R.id.selection);
-
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        // Создаем адаптер ArrayAdapter с помощью массива строк и стандартной разметки элемета spinner
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, viewDTP);
-        // Определяем разметку для использования при выборе элемента
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Применяем адаптер к элементу spinner
-        spinner.setAdapter(adapter);
-
-        AdapterView.OnItemSelectedListener itemSelectedListener = new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-                // Получаем выбранный объект
-                String item = (String)parent.getItemAtPosition(position);
-                selection.setText(item);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        };
-        spinner.setOnItemSelectedListener(itemSelectedListener);
     }
-
-    }
+}
--- app/src/main/java/space/sadnov/alenov/dtpapp/playerActivity.java
+++ app/src/main/java/space/sadnov/alenov/dtpapp/playerActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class playerActivity extends AppCompatActivity {
+public class pedestrian extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_player);
+        setContentView(R.layout.activity_pedestrian);
     }
 }
